6c081dded361c2a4e3916b9b9cf8b6e5914e4db9
Witold Andreasik <witold.andreasik@gmail.com>
2020-07-11 14:55:03 +0200

    add loading indicator component

---
+++ src/components/LoadingIndicator/LoadingIndicator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LoadingIndicator.css";
+function LoadingIndicator() {
+  return (
+    <div class="loading">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default LoadingIndicator;
